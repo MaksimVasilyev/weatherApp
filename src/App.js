18bf2f5a2b2946b560074e2e54e1f4745d3203cf
Maksim <vasilyev.v.maxim@gmail.com>
2023-04-06 13:37:01 -0600

    API key fixed

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   const [detailedDay, setDetailedDay] = useState(0);
 
   useEffect(() => {
-    const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/calgary%2C%20Canada?unitGroup=metric&key=VQ76ZQP5TD2GZEC47H54ZV3DB&include=obs%2Chours%2Cfcst%2Calerts&elements=datetime%2Ctemp%2Ctempmax%2Ctempmin%2Csunrise%2Csunset%2Cmoonphase%2Cconditions%2Cicon&aggregateHours=24&days=15`;
+    const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/calgary%2C%20Canada?unitGroup=metric&key=${process.env.REACT_APP_API_KEY}&include=obs%2Chours%2Cfcst%2Calerts&elements=datetime%2Ctemp%2Ctempmax%2Ctempmin%2Csunrise%2Csunset%2Cmoonphase%2Cconditions%2Cicon&aggregateHours=24&days=15`;
 
     fetch(url)
       .then(response => response.json())
