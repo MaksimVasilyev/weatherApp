6df24cf1bbb490893a23f62bde0c7c171c52c38e
Maksim <vasilyev.v.maxim@gmail.com>
2023-04-19 18:57:23 -0600

    code refactored

--- src/components/BlockOfDays.jsx
+++ src/components/BlockOfDays.jsx
@@ -1,64 +1,44 @@
 import React from "react";
-import Dayshort from './Dayshort'
+import Dayshort from './Dayshort';
 
+const BlockOfDays = ({ weatherData, firstDay, onDayClick, detailedDay }) => {
+  const getWeatherIcon = (condition) => {
+    const icons = {
+      "clear-day": "01d",
+      "clear-night": "01n",
+      "partly-cloudy-day": "02d",
+      "partly-cloudy-night": "02n",
+      "cloudy": "03d",
+      "rain": "10d",
+      "snow": "13d",
+      "thunderstorm": "11d",
+      "wind": "50d",
+      "fog": "50d"
+    };
+    return icons[condition] || "unknown.png";
+  };
 
+  const handleDayClick = (index) => {
+    onDayClick(index);
+  };
 
-function BlockOfDays(props) {
-
-  function getWeatherIcon(condition) {
-    switch (condition) {
-      case "clear-day":
-        return "01d";
-      case "clear-night":
-        return "01n";
-      case "partly-cloudy-day":
-        return "02d";
-        case "partly-cloudy-night":
-        return "02n";
-      case "cloudy":
-        return "03d";
-      case "rain":
-        return "10d";
-      case "snow":
-        return "13d";
-      case "thunderstorm":
-        return "11d";
-      case "wind":
-        return "50d";
-      case "fog":
-      return "50d";
-      default:
-        return "unknown.png";
-
-    }
-  }
-
-  function handleDayClick(index) {
-    props.onDayClick(index);
-  }
-  
-  
   return (
-    <div  style={{ display: "flex", justifyContent: "center", marginTop: "70px" }}>
-      {props.weatherData &&
-        props.weatherData.days.slice(props.firstDay, props.firstDay+5).map((day, index) => (
+    <div style={{ display: "flex", justifyContent: "center", marginTop: "70px" }}>
+      {weatherData &&
+        weatherData.days.slice(firstDay, firstDay + 5).map(({ datetime, icon, tempmin, tempmax }, index) => (
           <Dayshort
-            index={index+props.firstDay}
+            index={index + firstDay}
             key={index}
-            day={`${day.datetime.split("-")[2]}.${day.datetime.split("-")[1]}`}
-            img={`https://openweathermap.org/img/wn/${getWeatherIcon(day.icon)}@2x.png`}
-            temp={
-             <span>
-             {Math.round(day.tempmin)}&nbsp;&nbsp;&nbsp;{Math.round(day.tempmax)}
-             </span>
-                 }
-            style={{ marginRight: index === props.weatherData.days.length - 1 ? 0 : "15px" }}
+            day={`${datetime.split("-")[2]}.${datetime.split("-")[1]}`}
+            img={`https://openweathermap.org/img/wn/${getWeatherIcon(icon)}@2x.png`}
+            temp={<span>{Math.round(tempmin)}&nbsp;&nbsp;&nbsp;{Math.round(tempmax)}</span>}
+            style={{ marginRight: index === weatherData.days.length - 1 ? 0 : "15px" }}
             onDayClick={handleDayClick}
-            detailedDay={props.detailedDay}
+            detailedDay={detailedDay}
           />
         ))}
     </div>
-  )
-}
+  );
+};
 
 export default BlockOfDays;
--- src/components/Dayshort.jsx
+++ src/components/Dayshort.jsx
@@ -1,28 +1,24 @@
 import React from "react";
 
+const Dayshort = ({ index, onDayClick, detailedDay, day, img, temp }) => {
+  const handleClick = () => {
+    onDayClick(index);
+    console.log(detailedDay, index);
+  };
 
-function Dayshort(props) {
-
-  function handleClick() {
-    props.onDayClick(props.index);
-    console.log(props.detailedDay, props.index);
-  }
-
-  const frameStyle = props.index === props.detailedDay ? { 
-    border: "1px solid", 
+  const frameStyle = index === detailedDay ? {
+    border: "1px solid white",
     borderRadius: "15px",
     borderColor: "white",
-      } : {};  
-    return (
-    <div onClick={handleClick} style={{ alignItems: "center",  ...frameStyle}} >
-    <p style={{ marginBottom: 0, textAlign: "center" }}>{props.day}</p>
-    
-    <img style={{ marginBottom: 0,  marginTop: 0 }} src={props.img} alt="Weather icon" />
-    
-    <p style={{ marginTop: 0, textAlign: "center" }}>{props.temp}</p>
-  </div>
-    )
-}
+  } : {};
 
+  return (
+    <div onClick={handleClick} style={{ alignItems: "center", ...frameStyle }}>
+      <p style={{ marginBottom: 0, textAlign: "center" }}>{day}</p>
+      <img style={{ marginBottom: 0, marginTop: 0 }} src={img} alt="Weather icon" />
+      <p style={{ marginTop: 0, textAlign: "center" }}>{temp}</p>
+    </div>
+  );
+};
 
 export default Dayshort;
